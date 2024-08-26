35a34533ad985eb09deab00153e2ae74a6c61680
Robert Bubnik <rbubnik@gmail.com>
2024-08-25 20:43:00 -0400

    passing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+main = document.querySelector('#main')
+
+main.remove()
+
+const body = document.querySelector('body')
+const newHeader = document.createElement('h1')
+body.append(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Maria is the champion"
+
